fc0ed4dbe2c17d402a7cd25c51c670b5d97d082d
sym43 <remin0430@gmail.com>
2026-06-14 20:05:28 +0900

    Fix/ fix duplicate declaration
    업그레이드 데이터에 있던 oreamount 선언은 없앴습니다

--- Assets/Scripts/UpgradeSys/UpgradeData.cs
+++ Assets/Scripts/UpgradeSys/UpgradeData.cs
@@ -1,28 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 업그레이드에 들어가는 광물의 재료량을 의미.<br/><br/>
-/// 예시)<br/>
-/// [업그레이드]<br/>
-/// 채굴 속도 + 1<br/>
-/// 채굴 범위 + 1<br/>
-/// [재료]<br/>
-/// 철 - 10개 &lt;&lt;&lt; 이 광물량 하나를 의미.<br/>
-/// 구리 - 10개<br/>
-/// </summary>
-[System.Serializable]
-public struct OreAmount
-{
-    // public OreType oreType;
-    public int amount;
-    //
-    // public OreAmount(OreType oreType, int amount)
-    // {
-    //     this.oreType = oreType;
-    //     this.amount = amount;
-    // }
-}
+
 /// <summary>
 /// 업그레이드 노드를 구성하는 데이터.<br></br>
 /// SO이며, 노드 자체를 의미한다긴 보단 노드 뒤에서 주고받는 데이터라고 생각하면 됨.<br></br>

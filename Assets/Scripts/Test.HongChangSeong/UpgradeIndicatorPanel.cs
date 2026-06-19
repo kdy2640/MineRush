0849ce150af4367ab6df343c27da7034473ed270
hcs8697-glitch <hcs8697@gmail.com>
2026-06-19 17:21:23 +0900

    Chore : Modify UpgradePanelIndicator prefabs and scripts
    활성화 시 DOTween을 사용한 효과 나오게 구현.

--- Assets/Scripts/Test.HongChangSeong/UpgradeIndicatorPanel.cs
+++ Assets/Scripts/Test.HongChangSeong/UpgradeIndicatorPanel.cs
@@ -15,14 +15,20 @@ public class UpgradeIndicatorPanel : MonoBehaviour
     //UpgradeNodePanelController의 RefreshDescriptionPanel 메서드 내부가 수정되어야 한다.
     //왜냐하면, 광물은 이미지로 나올 것이고, 가격은 그대로 숫자로 나올 거니까.
 
-
+    [Header("출력할 부분")]
     [field: SerializeField] public TextMeshProUGUI DisplayNameText { get; private set; }
     [field: SerializeField] public TextMeshProUGUI DescriptionText { get; private set; }
     [field: SerializeField] public TextMeshProUGUI CostText { get; private set; } //이거 OreAmountText로 바꾸는 게?
     [field: SerializeField] public TextMeshProUGUI LevelText { get; private set; }
 
+    [Header("등장 시 패널 크기")]
+    [SerializeField] private float hoverScale = 1.2f;
+    [Header("연출 지속시간")]
+    [SerializeField] private float duration = 0.5f;
+
     private RectTransform rectTransform;
     private Vector3 originalScale;
+    private Vector3 originalRotation;
 
     private void Awake()
     {
@@ -32,7 +38,27 @@ public class UpgradeIndicatorPanel : MonoBehaviour
 
     private void OnEnable() //SetActive 방식으로 하셨기에 OnEnable을 써봤음.
     {
-        transform.DOKill();
-        rectTransform.DOScale(originalScale, 0.2f);
+        rectTransform.DOKill();
+
+        Sequence sequence = DOTween.Sequence();
+
+        //강의에서 시연받았던 방식이 아니라 From을 사용한 방식으로 작성됨.
+        sequence.Join(rectTransform.DOScale(originalScale, duration).From(originalScale * hoverScale).SetEase(Ease.OutCubic));
+
+        //sequence.Join(rectTransform.DOShakeRotation(duration, Vector3.forward * 10, 3, 2, true));
+
+        sequence.Join(rectTransform.DOPunchRotation(Vector3.forward * 15f, duration, 6, 0.5f));
+
+        sequence.OnComplete(() => //시퀀스가 종료되면, 명시적으로 회전값 0, 0, 0으로. 오류 방지.
+        {
+            rectTransform.localRotation = Quaternion.Euler(originalRotation);
+        });
+    }
+    private void OnDisable() //패널 비활성화 될 때
+    {
+        rectTransform.DOKill(); //해당 패널에 적용된 모든 효과를 끄고
+        rectTransform.localRotation = Quaternion.Euler(originalRotation); //rotation 값을 원본으로 돌림.
+
+        //UI는 rotation보다 localRotation을 사용하는 것이 안전하다고 함.
     }
 }

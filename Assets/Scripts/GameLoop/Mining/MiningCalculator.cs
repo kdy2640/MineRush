92497df02736f0d3034f0348c32493490e5cad4e
Doyeop Kim <rlaehduq2640@gmail.com>
2026-06-22 13:28:45 +0900

    Fix HitTiming

--- Assets/Scripts/GameLoop/Mining/MiningCalculator.cs
+++ Assets/Scripts/GameLoop/Mining/MiningCalculator.cs
@@ -7,8 +7,7 @@ public static class MiningCalculator
         RuntimeStat stat = GameManager.Instance.Upgrade.GetRuntimeStat();
 
         float random = Random.value;
-
-        // 예: 20% 확률로 아이템 지급 (0.2 이하)
+         
         if (random <= stat.CriticalChance)
         {
             return stat.MiningPower * stat.CriticalMultiplier;

751b837fb89d001ca1596479e4f96a1442622b4a
sym43 <remin0430@gmail.com>
2026-06-30 14:35:50 +0900

    Polish/ all of updateui and autominer

--- Assets/Scripts/UIs/PickAxe/PickaxeInfoPanel.cs
+++ Assets/Scripts/UIs/PickAxe/PickaxeInfoPanel.cs
@@ -11,6 +11,8 @@ public class PickaxeInfoPanel : MonoBehaviour
     [field: SerializeField] public TextMeshProUGUI CriticalChanceText { get; private set; }
     [field: SerializeField] public TextMeshProUGUI CostTitle { get; private set; }
     [field: SerializeField] public TextMeshProUGUI CostText { get; private set; }
+    [field: SerializeField] public GameObject RightMouseGuidePanel { get; private set; }
+    [SerializeField] private RectTransform targetLayoutRoot;
 
 
     //전달받은 데이터를 실제로 전시하는 역할.
@@ -18,12 +20,30 @@ public class PickaxeInfoPanel : MonoBehaviour
     {
         if (currentPickaxe == null)
             return;
+        int runtimeTier = GameManager.Instance.Upgrade.GetRuntimeStat().PickaxeTier;
+        bool isUnlocked = currentPickaxe.Tier <= runtimeTier;
 
-        DisplayNameText.text = currentPickaxe.DisplayName;
-        MiningPowerText.text = $"채굴 공격력 : {currentPickaxe.MiningPower}";
-        MiningSpeedText.text = $"채굴속도 : {currentPickaxe.MiningSpeed}";
-        MiningRadiusText.text = $"채굴반경 : {currentPickaxe.MiningRadius}";
-        CriticalChanceText.text = $"크리티컬 확률 : {currentPickaxe.CriticalChance}";
+        if (isUnlocked)
+        {
+            DisplayNameText.text = currentPickaxe.DisplayName;
+            MiningPowerText.gameObject.SetActive(true);
+            MiningSpeedText.gameObject.SetActive(true);
+            MiningRadiusText.gameObject.SetActive(true);
+            CriticalChanceText.gameObject.SetActive(true);
+            MiningPowerText.text = $"채굴 공격력 : {currentPickaxe.MiningPower}";
+            MiningSpeedText.text = $"채굴속도 : {currentPickaxe.MiningSpeed}";
+            MiningRadiusText.text = $"채굴반경 : {currentPickaxe.MiningRadius}";
+            CriticalChanceText.text = $"크리티컬 확률 : {currentPickaxe.CriticalChance * 100f:0.##}%";
+        }
+        else
+        {
+            DisplayNameText.text = "??????";
+            MiningPowerText.gameObject.SetActive(false);
+            MiningSpeedText.gameObject.SetActive(false);
+            MiningRadiusText.gameObject.SetActive(false);
+            CriticalChanceText.gameObject.SetActive(false);
+        }
+        
         if (CostText != null && CostTitle != null)
         {
             RefreshCostText(currentPickaxe);
@@ -33,20 +53,33 @@ public class PickaxeInfoPanel : MonoBehaviour
     public void RefreshCostText(PickaxesDataSO currentPickaxe)
     {
         UpgradeState state = GameManager.Instance.Upgrade.GetState(currentPickaxe.UpgradeData);
-        if (currentPickaxe.Tier != GameManager.Instance.Upgrade.GetRuntimeStat().PickaxeTier + 1)
+        int runtimeTier = GameManager.Instance.Upgrade.GetRuntimeStat().PickaxeTier;
+        CostText.gameObject.SetActive(false);
+        RightMouseGuidePanel.SetActive(false);
+        if (currentPickaxe.Tier == runtimeTier)
         {
-            CostTitle.gameObject.SetActive(false);
-            CostText.gameObject.SetActive(false);
-            return;
-        } // 이미 구매함.
+            CostTitle.text = "장착 중인 현재 곡갱이";
+        }
+        else if (currentPickaxe.Tier < runtimeTier)
+        {
+            CostTitle.text = "이미 해금했습니다.";
+        }
+        else if (currentPickaxe.Tier > runtimeTier + 1)
+        {
+            CostTitle.text = "이전 곡갱이를 먼저 해금해주세요.";
+        }
         else
         {
             CostTitle.gameObject.SetActive(true);
+            CostTitle.text = "비용 :";
             CostText.gameObject.SetActive(true);
             CostText.text = UpgradeOreCostTextFormatter.GetAllOreCostText(
                 state.GetCurrentCost(),
                 GameManager.Instance.OreManager
             );
+            RightMouseGuidePanel.SetActive(true);
         }
+        Canvas.ForceUpdateCanvases();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(targetLayoutRoot);
     }
 }
